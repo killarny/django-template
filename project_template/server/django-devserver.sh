de408af912896de16837bffb236fcdf5b507c7a1
Luke Graybill <luke@killarny.net>
2017-01-27 17:48:03 -0700

    delay starting devserver if the database isn't up

--- project_template/server/django-devserver.sh
+++ project_template/server/django-devserver.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
+until nc -z -v -w30 database 5432
+do
+  echo "Waiting for database container to start..."
+  sleep 5
+done
 python /{{ project_name }}/manage.py migrate --noinput
 python /{{ project_name }}/manage.py runserver 0.0.0.0:8000
