bcb0a7ae8de8c6167f0e27b0849ea706bc4ff6aa
Luke Graybill <luke@killarny.net>
2017-01-30 13:31:31 -0700

    use getattr on settings object, not .get()

--- project_template/project_name/context_processors.py
+++ project_template/project_name/context_processors.py
@@ -2,4 +2,4 @@ from django.conf import settings
 
 
 def site_name(request):
-    return {'site_name': settings.get('SITE_NAME', '{{ project_name }}')}
+    return {'site_name': getattr(settings, 'SITE_NAME', '{{ project_name }}')}
