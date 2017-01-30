62db42f44c5f2e1836237922ba6925617545fcb0
Luke Graybill <luke@killarny.net>
2017-01-30 14:21:46 -0700

    if the user is already logged in, redirect to root

--- project_template/apps/registration/views.py
+++ project_template/apps/registration/views.py
@@ -1,8 +1,12 @@
 from django.contrib.auth.views import login as auth_login, logout_then_login
+from django.shortcuts import redirect
 from django.urls import reverse
 
 
 def login(request):
+    # send the user to the root url if they're already logged in
+    if request.user.is_authenticated:
+        return redirect('root')
     response = auth_login(request)
     # we want to use the site_name from our context_processor instead
     response.context_data.pop('site_name')
