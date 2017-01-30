dfe760d314cee279788b7dc8c90d72f6b739f19f
Luke Graybill <luke@killarny.net>
2017-01-30 14:33:33 -0700

    if the facebook settings are missing, notify managers and the user

--- project_template/apps/registration/views.py
+++ project_template/apps/registration/views.py
@@ -1,4 +1,7 @@
+from django.conf import settings
+from django.contrib import messages
 from django.contrib.auth.views import login as auth_login, logout_then_login
+from django.core.mail import mail_managers
 from django.shortcuts import redirect
 from django.urls import reverse
 
@@ -10,6 +13,24 @@ def login(request):
     response = auth_login(request)
     # we want to use the site_name from our context_processor instead
     response.context_data.pop('site_name')
+    # if facebook key/secret are missing, we can't login
+    if not (settings.SOCIAL_AUTH_FACEBOOK_KEY or
+                settings.SOCIAL_AUTH_FACEBOOK_SECRET):
+        mail_managers('Facebook settings are missing or invalid!',
+                      'Someone tried to login on the website, but your '
+                      'Facebook settings are not filled out. Check your '
+                      'settings file and/or Docker environment!')
+        messages.error(request,
+                       'Unable to sign in right now. Please try again later!')
+        if settings.DEBUG:
+            # add some context data if the facebook key/secret are missing
+            response.context_data.update({
+                'facebook_error': "Facebook settings are not filled out. "
+                                  "Check your settings file and/or "
+                                  "Docker environment!",
+            })
+        else:
+            return redirect('root')
     return response
 
 
