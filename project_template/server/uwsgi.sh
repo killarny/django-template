09679e3053367ff015e63baf6fad24f2db0a0939
Luke Graybill <luke@killarny.net>
2017-01-31 15:08:44 -0700

    quieter static file collection

--- project_template/server/uwsgi.sh
+++ project_template/server/uwsgi.sh
@@ -4,6 +4,6 @@ do
   echo "Waiting for database container to start..."
   sleep 5
 done
-python /{{ project_name }}/manage.py collectstatic --noinput
+python /{{ project_name }}/manage.py collectstatic -v0 --noinput
 python /{{ project_name }}/manage.py migrate --noinput
 uwsgi /{{ project_name }}/server/uwsgi.ini
