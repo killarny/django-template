ade70ad20fdc34405fc1f3cf98a72afe9ef1868d
Luke Graybill <luke@killarny.net>
2017-01-31 15:12:39 -0700

    move static media collection above the db check
    the process takes a little bit, so we might as well do it
    before waiting for the database to come up

--- project_template/server/uwsgi.sh
+++ project_template/server/uwsgi.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
+
+echo "Collecting static files.."
+python /{{ project_name }}/manage.py collectstatic -v0 --noinput
+
 until nc -z -v -w30 database 5432 > /dev/null 2>&1
 do
   echo "Waiting for database container to start..."
   sleep 5
 done
-python /{{ project_name }}/manage.py collectstatic -v0 --noinput
+
 python /{{ project_name }}/manage.py migrate --noinput
 uwsgi /{{ project_name }}/server/uwsgi.ini
