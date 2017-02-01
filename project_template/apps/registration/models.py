132caad35be978629395adcf7dc4fda09169e137
Luke Graybill <luke@killarny.net>
2017-02-01 15:38:34 -0700

    use a signal to do the first user admin changes instead

--- project_template/apps/registration/models.py
+++ project_template/apps/registration/models.py
@@ -1,4 +1,7 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.mail import mail_managers
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class User(AbstractUser):
@@ -13,3 +16,18 @@ class User(AbstractUser):
     def image_url(self):
         return ('https://graph.facebook.com/{facebook_id}/picture'
                 '?type=large').format(facebook_id=self.facebook.uid)
+
+
+@receiver(post_save, sender=User)
+def first_user_is_admin(sender, instance, created, raw, using, update_fields):
+    # is this user being created, and is it the first (i.e., the only) user?
+    if created and sender.objects.count() == 1:
+        # give them full access
+        instance.is_superuser = True
+        instance.is_staff = True
+        instance.is_active = True
+        instance.save()
+        # tell the managers what just happened, via email
+        mail_managers('New superuser now has full site access.',
+                      'User "{username}" is the first user to sign up, and '
+                      'has been automatically granted superuser access.')
