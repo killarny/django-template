16391a9c255d1a9d33f4d0167e47eaee48c7bd51
Luke Graybill <luke@killarny.net>
2017-01-31 21:20:29 -0700

    remove unused import

--- project_template/project_name/urls.py
+++ project_template/project_name/urls.py
@@ -17,7 +17,6 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import login_required
 from django.templatetags.static import static as static_asset
 from django.views.generic.base import RedirectView
