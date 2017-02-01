90fa114902970f81d38e67709153de1638f1030d
Luke Graybill <luke@killarny.net>
2017-01-31 18:46:31 -0700

    validate the caddy config before starting caddy

--- project_template/server/proxy/caddy.sh
+++ project_template/server/proxy/caddy.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 envsubst < /opt/src/Caddyfile.template 1>/opt/src/Caddyfile
+if ! caddy -validate 2>/tmp/caddy-oops; then
+    cat /tmp/caddy-oops
+    exit 1
+fi
 caddy -agree -log=stdout
