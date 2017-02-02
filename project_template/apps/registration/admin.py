0f0bb7f14d29bc6dcc558950fd79610d8030e097
Luke Graybill <luke@killarny.net>
2017-02-02 11:39:39 -0700

    add a full name field in the user admin

--- project_template/apps/registration/admin.py
+++ project_template/apps/registration/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
+from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
 
 from .models import User
@@ -22,5 +23,13 @@ class UserAdmin(AuthUserAdmin):
             'fields': ('last_login', 'date_joined'),
         }),
     )
+    list_display = ('name_admin', 'username', 'email', 'is_staff')
     list_filter = ('is_staff', 'is_superuser', 'is_active')
     filter_horizontal = ('user_permissions',)
+
+    def name_admin(self, instance):
+        return mark_safe('<img src="{img}" height="12" />&nbsp;{name}'.format(
+            img=instance.image_url,
+            name=instance.get_full_name(),
+        ))
+    name_admin.short_description = u'name'
